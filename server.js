0315ab97ae313959c2822e908d8b87dcd2b6d2a6
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-11 20:29:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    socket.on('input', (data) => {
-        socket.broadcast.emit('input', data);
+    socket.on('createRoom', (roomId) => socket.join(roomId));
+    socket.on('joinRoom', (roomId) => socket.join(roomId));
+
+    // Синхронизация нажатий: отправляем всем в комнате, кроме отправителя
+    socket.on('inputSync', (data) => {
+        socket.to(data.roomId).emit('inputSync', data);
     });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log("Server active"));
+server.listen(process.env.PORT || 3000, () => console.log("Server running..."));
