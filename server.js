52b41bd419b2d8785fa61ce525154b762cd9a58a
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-14 01:08:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require("socket.io");
 const app = express();
-const server = http.createServer(app);
-
-// Разрешаем все соединения для Socket.io
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const path = require('path');
 
 app.use(express.static('public'));
 
-const rooms = {};
-
-io.on('connection', (socket) => {
-    console.log('Кто-то подключился:', socket.id);
-
-    socket.on('createRoom', (roomId) => {
-        rooms[roomId] = { host: socket.id };
-        socket.join(roomId);
-        console.log("Комната создана:", roomId);
-    });
-
-    socket.on('checkRoom', (roomId, callback) => {
-        if (rooms[roomId]) {
-            callback({ exists: true });
-        } else {
-            callback({ exists: false });
-        }
-    });
+// Если кто-то заходит на /arena, отдаем арену
+app.get('/arena', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/arena.html'));
 });
 
-server.listen(process.env.PORT || 3000, () => console.log('Сервер запущен'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('Арена готова!'));
