0536ae80673228ce85e1601868b6f589dcce42c1
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-14 00:29:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
 const app = express();
-const path = require('path');
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Раздаем статику из public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Обработка запуска
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Хранилище: { "ABCD": { host: "socketId" } }
+const rooms = {};
+
+io.on('connection', (socket) => {
+    socket.on('createRoom', (roomId) => {
+        rooms[roomId] = { host: socket.id };
+        socket.join(roomId);
+        console.log("Комната создана:", roomId);
+    });
+
+    socket.on('checkRoom', (roomId, callback) => {
+        if (rooms[roomId]) {
+            callback({ exists: true });
+        } else {
+            callback({ exists: false });
+        }
+    });
 });
+
+server.listen(process.env.PORT || 3000);
