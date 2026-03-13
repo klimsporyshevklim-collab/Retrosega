20bc2ffccb50a29fc7f9ff9b16723969c6e4467e
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-14 00:06:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
-// Просто раздаем папку public
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Бизнес-сервер запущен на порту ${PORT}`));
+// Тут живет состояние игры
+let gameState = { players: {} };
+
+io.on('connection', (socket) => {
+    // Регистрация игрока
+    socket.on('join', (id) => {
+        gameState.players[socket.id] = { x: 0, y: 0 };
+        socket.emit('init', gameState);
+    });
+
+    // Прием инпутов (КНОПКИ)
+    socket.on('input', (data) => {
+        // Сервер сам меняет состояние мира
+        // И рассылает всем обновленную картинку
+        io.emit('update', gameState);
+    });
+});
+
+server.listen(3000);
