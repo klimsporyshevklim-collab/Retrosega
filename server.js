a2382ed7e225097cf42d6cc0828424cfb0544b05
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-13 16:26:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,8 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
 
+// Просто раздаем папку public
 app.use(express.static('public'));
 
-const rooms = {};
-
-io.on('connection', (socket) => {
-    socket.on('createRoom', (roomId) => {
-        socket.join(roomId);
-        rooms[roomId] = { host: socket.id, guest: null, readyCount: 0 };
-    });
-
-    socket.on('joinRoom', (roomId) => {
-        if(rooms[roomId]) {
-            socket.join(roomId);
-            rooms[roomId].guest = socket.id;
-            // Команда обоим: "Начинайте скачивать игру в память!"
-            io.to(roomId).emit('startDownload'); 
-        }
-    });
-
-    // Игрок сообщает, что файл игры скачан
-    socket.on('playerReady', (roomId) => {
-        if(rooms[roomId]) {
-            rooms[roomId].readyCount++;
-            // Если ОБА игрока скачали игру - даем команду на одновременный запуск
-            if(rooms[roomId].readyCount === 2) {
-                io.to(roomId).emit('bootEngine');
-            }
-        }
-    });
-
-    // Пересылка кнопок
-    socket.on('inputSync', (data) => {
-        socket.to(data.roomId).emit('inputSync', data);
-    });
-});
-
-server.listen(process.env.PORT || 3000, () => console.log('Арена запущена'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Бизнес-сервер запущен на порту ${PORT}`));
