ef822b2e8dd4854c49ce9aa21b9a822419ef2644
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-15 18:47:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Указываем, что файлы лежат в public
+// Раздаем все файлы из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000, () => console.log('Арена запущена'));
+// Если зашли на /arena, отдаем арену
+app.get('/arena', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'arena.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Арена запущена на порту ' + PORT));
