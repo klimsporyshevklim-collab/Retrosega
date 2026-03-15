d380a566c5d8f207d593ee391252e089c165580d
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-15 15:26:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+app.use(express.static('public'));
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
-// Указываем, что файлы лежат в public
-app.use(express.static(path.join(__dirname, 'public')));
+// Сигнальный сервер: игроки обмениваются здесь только ID своих сессий
+io.on('connection', (socket) => {
+    socket.on('offer', (data) => socket.broadcast.emit('offer', data));
+    socket.on('answer', (data) => socket.broadcast.emit('answer', data));
+    socket.on('candidate', (data) => socket.broadcast.emit('candidate', data));
+});
 
-app.listen(process.env.PORT || 3000, () => console.log('Арена запущена'));
+server.listen(process.env.PORT || 3000, () => console.log('Сигнальный сервер запущен'));
